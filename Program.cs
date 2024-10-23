23a72468c4c3fdcd62f15e185c097fa8a4946107
Sam Blackburn <sam.blackburn@red-gate.com>
2024-10-23 11:08:48 +0100

    Filter out test projects

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using CsvHelper;
+
+using var reader = new StreamReader(args.FirstOrDefault() ?? @"../../../input/input.csv");
+using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+var records = csv.GetRecords<CsvRow>();
+var filteredRecords = new List<CsvRow>();
+foreach (var record in records)
+{
+    var filteredProjects = record.projects
+        .Split(", ")
+        .Where(p => !p.Contains("\\test", StringComparison.OrdinalIgnoreCase))
+        .ToArray();
+
+    if (!filteredProjects.Any()) continue;
+    
+    filteredRecords.Add(record with { projects = string.Join(", ", filteredProjects) });
+}
+
+Directory.CreateDirectory("../../../output");
+using var writer = new StreamWriter(File.Open("../../../output/output.csv", FileMode.Create));
+using var filteredCsv = new CsvWriter(writer, CultureInfo.CurrentCulture);
+filteredCsv.WriteRecords(filteredRecords);
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable NotAccessedPositionalProperty.Global
+public record CsvRow(
+    string id,
+    string name,
+    Version version,
+    PackageManager type,
+    int issuesCritical,
+    int issuesHigh,
+    int issuesMedium,
+    int issuesLow,
+    string dependenciesWithIssues,
+    string licenses,
+    string projects,
+    Uri licenseUrls,
+    Version latestVersion,
+    DateTime? latestVersionPublishedDate,
+    DateTime? firstPublishedDate,
+    bool? isDeprecated);
+
+public enum PackageManager { npm, nuget }
